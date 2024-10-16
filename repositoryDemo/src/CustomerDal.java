b5170fddc3a95ac8a8b29398a79000abdcabbce4
Bora Çiçek <100208097+boracicekk@users.noreply.github.com>
2024-10-16 17:44:16 +0300

    Add files via upload

---
+++ repositoryDemo/src/CustomerDal.java
@@ -0,0 +1,72 @@
+public class CustomerDal implements IEntitiyRepository <Customer>{
+    /*
+    @Override
+    public void add(CustomerDal entity) {
+
+    }
+
+    @Override
+    public void delete(CustomerDal entity) {
+
+    }
+
+    @Override
+    public void update(CustomerDal entity) {
+
+    }
+    */
+    /*
+    @Override
+    public void add(Validator entity) {
+
+    }
+
+    @Override
+    public void delete(Validator entity) {
+
+    }
+
+    @Override
+    public void update(Validator entity) {
+
+    }
+    */
+    //If we don't mention any type for IEntitiyRepository we take like this @Override automatically.
+    /*   @Override
+    public void add(Object entity) {
+
+    }
+
+    @Override
+    public void delete(Object entity) {
+
+    }
+
+    @Override
+    public void update(Object entity) {
+
+    }
+
+  */
+
+    //public class CustomerDal implements IEntitiyRepository <Customer> usage:
+
+    @Override
+    public void add(Customer entity) {
+
+    }
+
+    @Override
+    public void delete(Customer entity) {
+
+    }
+
+    @Override
+    public void update(Customer entity) {
+
+    }
+
+
+
+
+}
---
+++ repositoryDemo/src/Validator.java
@@ -0,0 +1,7 @@
+public class Validator {
+    //Generic Method usage:
+    
+    public <T extends IEntity> void validate(T entity){
+
+    }
+}
