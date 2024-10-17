a2f5c96d2d6b75d4cc3526fcf3147da0e3f7e38b
Bora Çiçek <100208097+boracicekk@users.noreply.github.com>
2024-10-17 22:18:04 +0300

    Add files via upload

---
+++ generics/src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    //MyList<String> list = new MyList<>();
+    //list.add("Ankara");
+    // This usage is wrong because that's not string.
+      //  list.add(1);
+
+        MyList<Customer> list = new MyList<>();
+        list.add(new Customer());
+        list.add(new Customer());
+
+    }
+}
