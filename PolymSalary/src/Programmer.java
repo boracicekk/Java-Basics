f584c9e2716c409845f9e8cabffa8c57dde24e04
Bora Çiçek <boracicekk@outlook.com>
2024-10-26 23:24:39 +0300

    Add files via upload

---
+++ PolymSalary/src/Programmer.java
@@ -0,0 +1,23 @@
+class Programmer extends Employee {
+
+    // Declare private double variables baseSalary and overtimePay
+    private double baseSalary;
+    private double overtimePay;
+
+    // Constructor for Programmer class that takes name, baseSalary, and overtimePay as parameters
+    public Programmer(String name, double baseSalary, double overtimePay) {
+        // Call the constructor of the superclass Employee with name and role "Programmer"
+        super(name, "Programmer");
+        // Assign the parameter baseSalary to the instance variable baseSalary
+        this.baseSalary = baseSalary;
+        // Assign the parameter overtimePay to the instance variable overtimePay
+        this.overtimePay = overtimePay;
+    }
+
+    // Override the calculateSalary method from the Employee class
+    @Override
+    public double calculateSalary() {
+        // Calculate and return the salary of the programmer by adding baseSalary and overtimePay
+        return baseSalary + overtimePay;
+    }
+}
